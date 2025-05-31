8fb13409418f30115acbd598b44eeb7b3a2ea810
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-05-30 22:00:58 -0300

    product movement api

---
+++ src/app/api/product/[id]/movements/route.ts
@@ -0,0 +1,20 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
+
+	try {
+		const productMovements = await prisma.stockMovement.findMany({
+			where: {productId: params.id},
+			orderBy: {createdAt: "desc"}
+		})
+
+		return NextResponse.json(productMovements)
+
+
+	} catch (error) {
+		console.error(error)
+		return NextResponse.json({ error: "Erro ao buscar movimentações" }, { status: 500 })
+	}
+
+}
