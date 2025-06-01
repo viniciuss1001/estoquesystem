b1f178f4c0c2c9996fb678a5a35e6637471dc566
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-06-01 20:54:17 -0300

    warehouse-product api

---
+++ src/app/api/warehouse-products/[id]/route.ts
@@ -0,0 +1,76 @@
+import { authOptions } from "@/lib/authOptions";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(req: NextRequest) {
+	try {
+
+		const session = await getServerSession(authOptions)
+
+		if (!session || session.user.office !== "ADMIN") {
+			return new Response("Unauthorized", { status: 401 })
+		}
+
+		const { searchParams } = new URL(req.url)
+		const warehouseId = searchParams.get("warehouseId")
+		const productId = searchParams.get("productId")
+
+		if (!warehouseId || !productId) {
+			return new Response("Parâmetros inválidos", { status: 400 })
+		}
+
+		const body = await req.json()
+
+		const { quantity } = body
+
+		const warehouseProductUpdated = await prisma.warehouseProduct.update({
+			where: {
+				warehouseId_productId: {
+					warehouseId, productId
+				}
+			},
+			data: {
+				quantity
+			}
+		})
+
+		return NextResponse.json(warehouseProductUpdated)
+
+	} catch (error) {
+		console.error(error)
+		return new Response("Erro ao atualizar", { status: 500 })
+	}
+}
+
+export async function DELETE(req: NextRequest) {
+	try {
+		const session = await getServerSession(authOptions);
+		const { searchParams } = new URL(req.url);
+		const warehouseId = searchParams.get("warehouseId");
+		const productId = searchParams.get("productId");
+
+		if (!session || session.user.office !== "ADMIN") {
+			return new Response("Unauthorized", { status: 401 });
+		}
+
+		if (!warehouseId || !productId) {
+			return new Response("Parâmetros inválidos", { status: 400 });
+		}
+
+		await prisma.warehouseProduct.delete({
+			where: {
+				warehouseId_productId: {
+					warehouseId,
+					productId
+				}
+			}
+		})
+
+		return new Response("Relação deletada com sucesso", { status: 200 })
+
+	} catch (error) {
+		console.error(error)
+		return new Response("Erro ao deletar", { status: 500 })
+	}
+}
---
+++ src/app/api/warehouse-products/route.ts
@@ -0,0 +1,58 @@
+import { authOptions } from "@/lib/authOptions";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+	try {
+		const session = await getServerSession(authOptions)
+
+		if (!session || session.user.office !== "ADMIN") {
+			return new Response("Unauthorized", { status: 401 })
+		}
+
+		const body = await req.json()
+
+		const { warehouseId, productId, quantity } = body
+
+		if (!warehouseId || productId) {
+			return new NextResponse("warehouseId e productId são obrigatórios", { status: 400 })
+		}
+
+		const warehouseProduct = await prisma.warehouseProduct.create({
+			data: {
+				warehouseId,
+				productId,
+				quantity: quantity ?? 0
+			}
+		})
+
+		return NextResponse.json(warehouseProduct)
+
+	} catch (error) {
+		console.error(error)
+		return new NextResponse("Erro ao criar relação armazém-produto", { status: 500 })
+	}
+}
+
+export async function GET( ){
+	try {
+		const session = await getServerSession(authOptions)
+
+		if (!session || session.user.office !== "ADMIN") {
+			return new Response("Unauthorized", { status: 401 })
+		}
+
+		const warehouseProducts = await prisma.warehouseProduct.findMany({
+			include: {
+				warehouse: true,
+				product: true
+			}
+		})
+
+		return NextResponse.json(warehouseProducts)
+
+	} catch (error) {
+		
+	}
+}
