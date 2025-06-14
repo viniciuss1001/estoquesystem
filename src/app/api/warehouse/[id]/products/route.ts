91c879569e8cf1d6bc9d7dc8b7e425c5a1d13604
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-06-14 20:28:01 -0300

    feature: products in warehouse api

---
+++ src/app/api/warehouse/[id]/products/route.ts
@@ -0,0 +1,33 @@
+import { authOptions } from "@/lib/authOptions";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function name(_: NextRequest, { params }: { params: { id: string } }) {
+
+	try {
+
+		const session = await getServerSession(authOptions)
+
+		if (!session) {
+			return new NextResponse("Não autorizado", { status: 401 })
+		}
+
+		const productsInWarehouse = await prisma.warehouseProduct.findMany({
+			where: {warehouseId: params.id},
+			include: {
+				product: {
+					include: {
+						supplier: true
+					}
+				}
+			}
+		})
+
+		return NextResponse.json(productsInWarehouse)
+
+	} catch (error) {
+		console.error(error)
+		return new NextResponse("Erro ao buscar produtos do armazém", { status: 500 })
+	}
+}
