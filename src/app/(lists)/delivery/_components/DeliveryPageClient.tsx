3ef2881989d2bbee997862fc1784ce77599a19ec
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-08-14 08:17:20 -0300

    fix: lint page I

---
+++ src/app/(lists)/delivery/_components/DeliveryPageClient.tsx
@@ -0,0 +1,140 @@
+"use client"
+
+import CreateDeliveryForm from '@/app/(lists)/delivery/_components/CreateDeliveryForm'
+import DeliveryFilterDialog from '@/app/(lists)/delivery/_components/DeliveryFilterDialog'
+import DeliveryStatusBadge from '@/app/(lists)/delivery/_components/DeliveryStatusBadge'
+import EditDeliveryModal from '@/app/(lists)/delivery/_components/EditDeliveryModal'
+import { Button } from '@/components/ui/button'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import api from '@/lib/axios'
+import { useFilteredDeliveries } from '@/lib/queries'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { format } from 'date-fns'
+import { Loader2, Trash } from 'lucide-react'
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
+import { toast } from 'sonner'
+
+
+const DeliveryPageClient = () => {
+
+	const queryClient = useQueryClient()
+	const searchParams = useSearchParams()
+
+	const validStatuses = ["PENDING", "COMPLETED", "CANCELED", "LATE"] as const
+
+	const rawStatus = searchParams.get("status")
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const status = validStatuses.includes(rawStatus as any) 
+		? (rawStatus as typeof validStatuses[number])
+		: undefined
+
+	const productId = searchParams.get("productId") || undefined
+	const supplierId = searchParams.get("supplierId") || undefined
+	const warehouseId = searchParams.get("warehouseId") || undefined
+
+
+	const { data: deliveries = [], isLoading } = useFilteredDeliveries({
+		productId,
+		supplierId,
+		warehouseId,
+		status
+	})
+
+	const deleteDelivery = useMutation({
+		mutationFn: async (id: string) => {
+			return await api.delete(`/delivery/${id}`)
+		},
+		onSuccess: () => {
+			toast.success("Entrega excluída com sucesso!")
+			queryClient.invalidateQueries({ queryKey: ["deliveries"] })
+		},
+		onError: () => {
+			toast.error("Erro ao excluir entrega")
+		}
+	})
+
+	const handleDelete = async (id: string) => {
+		deleteDelivery.mutate(id)
+	}
+
+	if (isLoading) return <div className='flex items-center justify-center w-full h-full'><Loader2 className='animate-spin' /></div>
+
+
+	return (
+		<div className='p-6 '>
+			<div className="flex justify-between items-center mb-4">
+				<div className='flex flex-col gap-2'>
+				<h2 className="text-2xl font-bold">Entregas</h2>
+					<p className='text-sm text-muted-foreground'>
+						Total de {deliveries.length} entrega (s) sendo exibidas.
+					</p>
+				</div>
+				<div className="flex gap-2">
+					<DeliveryFilterDialog />
+					<CreateDeliveryForm />
+				</div>
+			</div>
+
+
+			{deliveries.length === 0 ? (<p>Nenhuma entrega cadastrada.</p>) : (
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead>Produto</TableHead>
+							<TableHead>Fornecedor</TableHead>
+							<TableHead>Quantidade</TableHead>
+							<TableHead>Armazém</TableHead>
+							<TableHead>Data prevista</TableHead>
+							<TableHead>Boleto</TableHead>
+							<TableHead>Estado</TableHead>
+							<TableHead className="text-right">Ações</TableHead>
+							<TableHead>Detalhes</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{deliveries.map((delivery) => (
+							<>
+								<TableRow key={delivery.id}>
+									<TableCell>{delivery.product.name}</TableCell>
+									<TableCell>{delivery.supplier.name}</TableCell>
+									<TableCell>{delivery.quantity}</TableCell>
+									<TableCell>
+										{delivery.warehouse.name}
+									</TableCell>
+									<TableCell>{format(new Date(delivery.expectedAt), "dd/MM/yyyy")}</TableCell>
+									<TableCell>
+
+										{delivery.supplierInvoice?.title ?? "-"}
+									</TableCell>
+									<TableCell>
+										<DeliveryStatusBadge status={delivery.status} />
+									</TableCell>
+									<TableCell className="flex items-center justify-end gap-2">
+										<EditDeliveryModal
+											deliveryId={delivery.id}
+										/>
+										<Button variant="destructive" onClick={() => handleDelete(delivery.id)}
+											className='cursor-pointer flex gap-2'
+										>
+											<Trash className="w-4 h-4" />
+											Deletar
+										</Button>
+									</TableCell>
+									<TableCell>
+										<Link href={`/delivery/${delivery.id}`}>
+											Detalhes
+										</Link>
+									</TableCell>
+								</TableRow>
+							</>
+						))}
+					</TableBody>
+				</Table>
+			)}
+		</div>
+	)
+}
+
+export default DeliveryPageClient
--- src/app/(password)/reset-password/page.tsx
+++ src/app/(password)/reset-password/page.tsx
@@ -1,11 +1,18 @@
+import { Suspense } from "react";
 import ResetPasswordForm from "@/app/(password)/_components/ResetPasswordForm";
-
 
 export default function ResetPasswordPage() {
   return (
-    <div className="max-w-md mx-auto h-fit mt-20 space-y-6 p-6 border rounded-lg shadow">
-      <h1 className="text-xl font-bold">Redefinir senha</h1>
-      <ResetPasswordForm />
-    </div>
+    <Suspense fallback={
+      <div>
+        Carregando...
+      </div>
+    }>
+
+      <div className="max-w-md mx-auto h-fit mt-20 space-y-6 p-6 border rounded-lg shadow">
+        <h1 className="text-xl font-bold">Redefinir senha</h1>
+        <ResetPasswordForm />
+      </div>
+    </Suspense>
   )
 }
