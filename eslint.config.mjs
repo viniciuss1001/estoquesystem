16ac0f248c8b1f3def5783dfe22eefd84fae2b10
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-08-14 08:04:50 -0300

    linfix: end I

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "dist/**",
+      "build/**",
+      "src/generated"
+    ],
+  },
 ];
 
 export default eslintConfig;
