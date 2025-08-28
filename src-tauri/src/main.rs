25824a4efad5afc16717155be1e667bf8aed61f5
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-08-27 22:20:08 -0300

    update prisma and tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,19 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-use tauri::api::path::app_dir;
-use std::path::PathBuf;
-
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
-}
+use tauri::api::path::app_dir;
+use std::path::PathBuf;
 
 #[tauri::command]
 fn get_db_path() -> String {
     let base_dir: PathBuf = app_dir(&tauri::Config::default()).unwrap();
-    let db_path = base_dir.join("data.db");
+    let db_path = base_dir.join("dev.db");
     db_path.to_string_lossy().into_owned()
+}
+
+fn main() {
+    tauri::Builder::default()
+        // registrar o comando
+        .invoke_handler(tauri::generate_handler![get_db_path])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
